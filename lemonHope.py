0bf80abdead6c01b94be2e547c0707f69e093213
Serubin <Serubin@serubin.net>
2021-04-10 21:18:27 -0700

    Seperate out sendQuote. Add variable consistency

--- lemonHope.py
+++ lemonHope.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import asyncio
 import re
 
+print('Lemon is starting')
 load_dotenv()
 token = os.getenv('lemonhope_token')
 
@@ -21,41 +22,56 @@ def isAlreadyRemembered(table, author, msg):
     return any(table.search(query.name.matches('.*' + author + '.*', flags=re.IGNORECASE) and query.message == msg))
 
 
+async def saveQuote(table, author, message, sendResponse):
+    if not isAlreadyRemembered(table, author, message):
+        table.insert({'name': author, 'message': message})
+        print(sendResponse)
+        await sendResponse('Remembered that ' + author + ' said ' + message)
+
+
+
 @lemon.event
-async def on_reaction_add(reaction, user):
-    if str(reaction.emoji) == '💬' and not any(r.me is True for r in reaction.message.reactions):
-        await reaction.message.add_reaction('💬')
+async def on_reaction_add(ctx, user):
+    if str(ctx.emoji) == '💬' and not any(r.me is True for r in ctx.message.reactions):
+        print('Saving quote from ' + ctx.message.author.name + ' via reaction')
+
         lock = asyncio.Lock()
+        quotepocket = getDBFromGuild(str(ctx.message.guild)).table('quote')
+
         await lock.acquire()
         try:
-            quotepocket = getDBFromGuild(str(reaction.message.guild)).table('quote')
-            if not isAlreadyRemembered(quotepocket, reaction.message.author.name, reaction.message.content):
-                quotepocket.insert({'name': reaction.message.author.name, 'message': reaction.message.content})
-                await reaction.message.channel.send(
-                    'Remembered that ' + reaction.message.author.name + ' said ' + reaction.message.content)
+            await saveQuote(
+                    quotepocket, ctx.message.author.name, ctx.message.content, ctx.message.channel.send)
         finally:
             lock.release()
+
+        await ctx.message.add_reaction('💬')
 
 
 @lemon.command()
 async def remember(ctx, *, arg):
     split = arg.split(' ')
+
     name = split[0]
     channel = ctx.message.channel
     findString = ''.join(split[1:])
-    messages = await channel.history(limit=50).flatten()
     found = False
+
+    print('Saving quote from ' + name + ' via text command')
+
+    messages = await channel.history(limit=50).flatten()
     quotepocket = getDBFromGuild(str(ctx.message.guild)).table('quote')
+
+
     for ms in messages:
         if name in ms.author.name and (findString in ms.content or not findString) and "Lemon, " not in ms.content:
             lock = asyncio.Lock()
             await lock.acquire()
             try:
-                if not isAlreadyRemembered(quotepocket, ms.author.name, ms.content):
-                    quotepocket.insert({'name': ms.author.name, 'message': ms.content})
-                    await ctx.send('Remembered that ' + ms.author.name + ' said "' + ms.content + '"!')
+                await saveQuote(quotepocket, ms.author.name, ms.content, ctx.send)
             finally:
                 lock.release()
+
             found = True
             break
     if not found:
