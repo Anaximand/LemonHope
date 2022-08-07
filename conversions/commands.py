962f60fbbe31b656548a6ba887c9db72d141fe45
Serubin <Serubin@serubin.net>
2022-08-07 19:03:25 -0400

    Clean up conversions a bit

--- conversions/commands.py
+++ conversions/commands.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from conversions.utils import getConversionTupleFromMessage, reMatchToTuple, convertMatch, buildConvertionSegment
+from conversions.utils import getConversionTupleFromMessage, convertMatch, buildConvertionStr
 
 class Conversions(commands.Cog):
     def __init__(self, bot):
@@ -12,30 +12,24 @@ class Conversions(commands.Cog):
 
         content = message.content
 
-        messageMatches = getConversionTupleFromMessage(content)
-        matchTuples = list(map(reMatchToTuple, messageMatches))
+        matchTuples = getConversionTupleFromMessage(content)
 
         if len(matchTuples) == 0:
             return
 
-        convStrs = []
-        for idx, matchTuple in enumerate(matchTuples):
-            convTuple = convertMatch(matchTuple)
-            if not convTuple:
-                continue
-
-            convStrs.append(buildConvertionSegment(matchTuple, convTuple))
-
-        if len(convStrs) == 0:
-            return
+        allConversions = list(map(convertMatch, matchTuples))
+        allConversionsLen = len(allConversions)
 
         response = ''
-        for idx, convStr in enumerate(convStrs):
+        for idx, conversion in enumerate(allConversions):
+            ogData, convertedData = conversion
+            convStr = buildConvertionStr(ogData, convertedData)
+
             messageSegment = ''
-            if len(convStrs) == 1 or idx == 0:
+            if allConversionsLen == 1 or idx == 0:
                 messageSegment = convStr
-            elif len(convStrs) - 1 == idx:
-                comma = '' if len(convStrs) == 2 else ','
+            elif allConversionsLen - 1 == idx:
+                comma = '' if allConversionsLen == 2 else ','
                 messageSegment += '{comma} and {conversion}'.format(comma=comma, conversion=convStr)
             else:
                 messageSegment = ', {conversion}'.format(conversion=convStr)
