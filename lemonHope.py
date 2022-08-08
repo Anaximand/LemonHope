26f3998e524585a4316531d24d72d944b72cbe0d
Serubin <Serubin@serubin.net>
2022-08-07 21:22:02 -0400

    Add better logging

--- lemonHope.py
+++ lemonHope.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -7,17 +8,25 @@ import quotes.commands as quotes
 import legs.commands as legs
 import conversions.commands as conversions
 
+logging.getLogger("discord").setLevel(logging.WARNING)
+
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(name)s: %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 def run():
-    print('Lemon is starting')
+    logger.info('Lemon is starting')
     load_dotenv()
     token = os.getenv('lemonhope_token')
 
-    lemon = commands.Bot(command_prefix="Lemon, ")
+    lemon = commands.Bot(command_prefix="Lemon, ", log_handler=None)
 
     quotes.setup(lemon)
     legs.setup(lemon)
     conversions.setup(lemon)
 
     lemon.run(token)
+    logger.info('Lemon has started')
 
 run()
